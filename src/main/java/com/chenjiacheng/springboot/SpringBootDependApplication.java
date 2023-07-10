9cc8f787370600fd2bb238182eb21b22324606cc
chenjiacheng <chen-jiacheng@foxmail.com>
2023-07-10 09:21:43 +0800

    # add unittest code

--- src/main/java/com/chenjiacheng/springboot/SpringBootDependApplication.java
+++ src/main/java/com/chenjiacheng/springboot/SpringBootDependApplication.java
@@ -2,9 +2,6 @@ package com.chenjiacheng.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * create by chenjiacheng on 2023/7/10 08:54
---
+++ src/test/java/com/chenjiacheng/springboot/SpringBootDependApplicationTest.java
@@ -0,0 +1,20 @@
+package com.chenjiacheng.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+/**
+ * create by chenjiacheng on 2023/7/10 09:18
+ *
+ * @author chenjiacheng
+ * @since 1.0.0
+ */
+@SpringBootTest
+public class SpringBootDependApplicationTest {
+
+    @Test
+    public void mainTest() {
+        System.out.println("hello");
+    }
+}
